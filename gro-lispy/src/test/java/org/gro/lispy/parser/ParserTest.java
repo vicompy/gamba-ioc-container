d761be830f617dd58fb8fc6871858ec30782b600
lechouga <lechouga@28f42034-b80a-11de-b71e-ab0a53e69959>
2009-12-10 15:43:59 +0000

    funcs

--- gro-lispy/src/test/java/org/gro/lispy/parser/ParserTest.java
+++ gro-lispy/src/test/java/org/gro/lispy/parser/ParserTest.java
@@ -8,20 +8,20 @@ public class ParserTest {
 	@Test
 	public void test1() {
 
-//		final String p = "( (+ 1 2 (+ 3 ZERO 4)))";
-//		final String p = "( (and 1 1 1 1 1 1 1 1)(and 1 1 1 1 ZERO 1))";
-//		final String p = "( ('(1 2 3)) (' ZERO) (disp version ('(3 4 5))))";
-//		final String p = "( ('(1 2 3)) (' 1))";
-//		final String p = "( (lambda (' x) ('(+ x x)))  )";
-//		final String p = "( (+ 3.14159 2)  )";
-//		final Parser parser = new Parser(p);
-//		parser.parseProgram();
-
+		// aggregates basics: nested, vars, stupid expressions
 		assertEquals("[12]", parse("( (+ 1 2 3 (+ 1 2 3)) )"));
+		assertEquals("[12]", parse("( (+ 1 (+ 1 2 3)2 3 ) )"));
+		assertEquals("[12]", parse("( (+ (+ 1 2 3)1 2 3 ) )"));
 		assertEquals("[1.0]", parse("( (+ version ZERO) )"));
 		assertEquals("[1]", parse("( (+ 1) )"));
 		assertEquals("[0]", parse("( (+) )"));
 
+		// longints vs. doubles
+		assertEquals("[3]", parse("( (+ 1 2) )"));
+		assertEquals("[3.0]", parse("( (+ 1.0 2) )"));
+		assertEquals("[3.0]", parse("( (+ 1 2.0) )"));
+
+		// quotes
 		assertEquals("[[1, 2, 3]]", parse("((quote (1 2 3)))"));
 		assertEquals("[[1, 2, 3]]", parse("((' (1 2 3)))"));
 
