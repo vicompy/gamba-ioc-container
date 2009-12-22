8f68e8274bb6cc0cccbecdfcb666951730cadbe5
lechouga <lechouga@28f42034-b80a-11de-b71e-ab0a53e69959>
2009-12-22 20:36:28 +0000

    AlphaBeta2 funka!! quina diferència

---
+++ trunk/chess/src/main/java/org/gro/chess/AlphaBeta2.java
@@ -0,0 +1,106 @@
+package org.gro.chess;
+
+import java.util.List;
+
+public class AlphaBeta2 implements ISearch {
+
+	private Node bestNode;
+	private long bestScore;
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.gro.chess.ISearch#getBestScore()
+	 */
+	public long getBestScore() {
+		return bestScore;
+	}
+
+	private long nodesAnalitzats;
+	private MapHash<Node> hash;
+
+	boolean usaHash;
+
+	public AlphaBeta2(final boolean usaHash) {
+		super();
+		this.usaHash = usaHash;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.gro.chess.ISearch#search(org.gro.chess.Node, int, int)
+	 */
+	public Node search(final Node node, final int maxDepth, final int myDir) {
+		this.bestNode = null;
+		this.bestScore = Long.MIN_VALUE;
+		this.nodesAnalitzats = 0L;
+		hash = new MapHash<Node>();
+
+		search(node, maxDepth, maxDepth, Long.MIN_VALUE, Long.MAX_VALUE, myDir, myDir);
+		System.out.println("analitzats* " + nodesAnalitzats + " nodes.");
+
+		return this.bestNode;
+	}
+
+	private long search(final Node node, final int initialDepth, final int depth, long alfa, long beta,
+			final int myDir, final int maximizingDir) {
+
+		nodesAnalitzats++;
+
+		if (depth == 0) {
+			return BoardHeuristic.calc(node, maximizingDir);
+		}
+
+		final MovGen movGen = new MovGen(node, myDir);
+		final List<Node> childs = movGen.generaMovesMatadors();
+		childs.addAll(movGen.generaMovesNoMatadors());
+
+		if (childs.size() == 0) {
+			return BoardHeuristic.calc(node, maximizingDir);
+		}
+
+		if (myDir == maximizingDir) {
+
+			if (initialDepth == depth) {
+				System.out.print("rumiant: 0%, ");
+			}
+			for (final Node child : childs) {
+				final long score = search(child, initialDepth, depth - 1, alfa, beta, -myDir, maximizingDir);
+				if (initialDepth == depth) {
+					if (bestScore < score) {
+						bestScore = score;
+						bestNode = child;
+					}
+				}
+				if (score > alfa) {
+					alfa = score;
+				}
+				if (alfa >= beta) {
+					return alfa; // cut-off
+				}
+				if (initialDepth == depth) {
+					System.out.print((childs.indexOf(child) * 100) / childs.size() + "%, ");
+				}
+			}
+			if (initialDepth == depth) {
+				System.out.println("OK");
+			}
+			return alfa; // our best move
+		} else {
+
+			for (final Node child : childs) {
+				final long score = search(child, initialDepth, depth - 1, alfa, beta, -myDir, maximizingDir);
+				if (score < beta) {
+					beta = score;
+				}
+				if (alfa >= beta) {
+					return beta; // cut-off
+				}
+			}
+			return beta; // our best move
+		}
+
+	}
+
+}
--- trunk/chess/src/test/java/org/gro/chess/SearchUsageTest.java
+++ trunk/chess/src/test/java/org/gro/chess/SearchUsageTest.java
@@ -22,60 +22,39 @@ public class SearchUsageTest {
 	"T.AQ.TK.";
 
 
-	Node n = new Node(board);
+
 
 	@Test
 	public void test1() {
 
+		Node n = new Node(board);
 		final ISearch ab = new Minimax(false);
-//		System.out.println(n);
-		try {
-			for (int i = 0; i < 4; i++) {
-				n = ab.search(n, 3, Node.WHITE_DIR);
-				n = ab.search(n, 3, Node.BLACK_DIR);
-//				System.out.println(n = ab.search(n, 4, Node.WHITE_DIR));
-//				System.out.println(n = ab.search(n, 4, Node.BLACK_DIR));
-			}
-		} catch (final Exception e) {
+		for (int i = 0; i < 4; i++) {
+			n = ab.search(n, 3, Node.WHITE_DIR);
+			n = ab.search(n, 3, Node.BLACK_DIR);
 		}
-
-//	}
-//
-//	@Test
-//	public void test2() {
 
 		Node n2 = new Node(board);
-		final ISearch ab2 = new AlphaBeta(false);
-//		System.out.println(n2);
-		try {
-			for (int i = 0; i < 4; i++) {
-				n2 = ab2.search(n2, 3, Node.WHITE_DIR);
-				n2 = ab2.search(n2, 3, Node.BLACK_DIR);
-//				System.out.println(n = ab.search(n, 4, Node.WHITE_DIR));
-//				System.out.println(n = ab.search(n, 4, Node.BLACK_DIR));
-			}
-		} catch (final Exception e) {
+		final ISearch ab2 = new AlphaBeta2(false);
+		for (int i = 0; i < 4; i++) {
+			n2 = ab2.search(n2, 3, Node.WHITE_DIR);
+			n2 = ab2.search(n2, 3, Node.BLACK_DIR);
+		}
+
+		Node n3 = new Node(board);
+		final ISearch ab3 = new AlphaBeta2Hash(false);
+		for (int i = 0; i < 4; i++) {
+			n3 = ab3.search(n3, 3, Node.WHITE_DIR);
+			n3 = ab3.search(n3, 3, Node.BLACK_DIR);
 		}
 
 
 
-//		Node n3 = new Node(board);
-//		final AlphaBeta ab3 = new AlphaBeta(true);
-////		System.out.println(n2);
-//		try {
-//			for (int i = 0; i < 2; i++) {
-//				n3 = ab3.search(n3, 3, Node.WHITE_DIR);
-//				n3 = ab3.search(n3, 3, Node.BLACK_DIR);
-////				System.out.println(n = ab.search(n, 4, Node.WHITE_DIR));
-////				System.out.println(n = ab.search(n, 4, Node.BLACK_DIR));
-//			}
-//		} catch (final Exception e) {
-//		}
+
 
 		assertEquals(n, n2);
-//		assertEquals(n, n3);
+		assertEquals(n, n3);
 
-//		assertEquals(n, n2);
 	}
 
 }
